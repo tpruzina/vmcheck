a0a80971600d102b26d5be904c4851be0401cf45
Tomáš Pružina <pruzinat@gmail.com>
2014-08-31 17:52:23 +0200

    initial commit

---
+++ vmcheck.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <inttypes.h>
+
+typedef struct
+{
+	unsigned long	limit : 16;
+	unsigned long	base  : 64;
+} __attribute__((packed)) dt;
+
+inline int
+idtCheck()
+{
+	dt idt = {0, 0};
+	asm volatile( "sidt %0" : "=m"(idt) );
+
+	return idt.base;
+}
+
+int
+gdtCheck()
+{
+	dt gdt = {0, 0};
+	asm volatile( "sgdt %0" : "=m"(gdt) );
+	
+	return gdt.base;
+}
+
+int
+ldtCheck()
+{
+	unsigned char m[6];
+
+	asm("sldt %0\n" : : "m"(m));
+
+	return (m[0] != 0x00 && m[1] != 0x00) ? 1 : 0;
+}
+
+
+int
+main()
+{
+	idtCheck();
+	gdtCheck();
+	if(ldtCheck())
+		return 0;
+	else
+		return -1;
+}
